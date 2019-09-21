01b31b73f5088a16cf95baf0066f9e1bde8c537c
Alvaro Segura Del Barco <alvarosb@gmail.com>
2019-09-20 23:25:43 -0600

    adds initial tests

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,4 +1,5 @@
 import os
+import pytest
 
 import testinfra.utils.ansible_runner
 
@@ -13,3 +14,28 @@ def test_hosts_file(host):
     assert f.exists
     assert f.user == 'root'
     assert f.group == 'root'
+
+
+@pytest.mark.parametrize('pkg', [
+    'razor-server',
+    'rubygems',
+    'ipmitool'
+])
+def test_installed_pkgs(host, pkg):
+    package = host.package(pkg)
+
+    assert package.is_installed
+
+
+@pytest.mark.parametrize('file', [
+    'README',
+    'SHA256SUM',
+    'initrd0.img',
+    'vmlinuz0'
+])
+def test_microkernel_files(host, file):
+    file = host.file(
+        '/opt/puppetlabs/server/data/razor-server/repo/microkernel/' + file
+    )
+
+    assert file.exists
